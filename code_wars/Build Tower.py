790e722e3fb36f34261df37bd8c4d97e78a73546
Paul <31617131+nochevnoy@users.noreply.github.com>
2018-10-10 12:01:28 +0700

    Update Build Tower.py

--- code_wars/Build Tower.py
+++ code_wars/Build Tower.py
@@ -1,8 +1,7 @@
 def tower_builder(n_floors):
         floors = []
-        n = n_floors
         for i in range(n_floors):
-            n -= 1
+            n_floors -= 1
             floors.append(' ' * n + '*' * (i * 2 + 1) + ' ' * n)
 
         return floors
