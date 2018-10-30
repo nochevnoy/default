851a48cf74b3bb18203d0fe19d02859527768102
Paul <31617131+nochevnoy@users.noreply.github.com>
2018-10-30 16:36:06 +0700

    Update lambda_functions.py

--- study/lambda_functions.py
+++ study/lambda_functions.py
@@ -4,4 +4,14 @@ def f(x):
 s.sort(key = f)
 
 #Можно заменить на:
+
 s.sort(key = lambda x: str(x)[-1])
+
+######################################
+def g(x):
+    return x[::-1]
+s = list(map(g, s))
+
+#Можно заменить на:
+
+s = list(map(lambda x: x[::-1], s))
